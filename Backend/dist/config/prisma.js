2c1cb2aae51cb03992cf1b7140d0e699d5f1ee2a
Soumik <soumikd998@gmail.com>
2026-05-27 04:56:21 +0530

    fixed Backend

--- Backend/dist/config/prisma.js
+++ Backend/dist/config/prisma.js
@@ -1,8 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { env } from './env.js';
-const pool = new Pool({ connectionString: env.DATABASE_URL });
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
 export const prisma = new PrismaClient({ adapter });
 //# sourceMappingURL=prisma.js.map
